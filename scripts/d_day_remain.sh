a1943321c6ea1a4b179baa0a4ccab0bf5b6e6a51
Posquit0.BJ <posquit0.bj@gmail.com>
2016-01-24 19:39:40 +0900

    add function for calculating a remains of D-Day

---
+++ scripts/d_day_remain.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+source "$CURRENT_DIR/helpers.sh"
+
+# Global Variables
+is_overdue="true"
+
+get_d_day_setting() {
+  d_day_remain_prefix=$(get_tmux_option "@d_day_remain_prefix" "D")
+  d_day_remain_postfix=$(get_tmux_option "@d_day_remain_postfix" "")
+  d_day_remain_plus_sign=$(get_tmux_option "@d_day_remain_plus_sign" "+")
+  d_day_remain_minus_sign=$(get_tmux_option "@d_day_remain_minus_sign" "-")
+  d_day_the_day=$(get_tmux_option "@d_day_the_day" "D-Day")
+}
+
+date_diff() {
+  local d1=$(date -d "$1" +%s)
+  local d2=$(date -d "$2" +%s)
+
+  if ((d1 < d2)); then
+    is_overdue="false"
+  else
+    is_overdue="true"
+  fi
+  return $(( ((d1 - d2) > 0 ? (d1 - d2) : (d2 - d1)) / (60 * 60 * 24) ))
+}
+
+print_d_day_remain() {
+  local today="today"
+  local d_day="2016-02-24"
+
+  date_diff $today $d_day
+  local days=$?
+
+  if (( days == 0 )); then
+    echo "$d_day_the_day"
+  else
+    if [ $is_overdue != "true" ]; then
+      local days="$d_day_remain_minus_sign$days"
+    else
+      local days="$d_day_remain_plus_sign$days"
+    fi
+    echo "$d_day_remain_prefix$days$d_day_remain_postfix"
+  fi
+}
+
+main() {
+  get_d_day_setting
+  print_d_day_remain
+}
+
+main
